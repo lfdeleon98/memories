00c6a01033e9c416036ac1cf44cd123b7175de2a
lauren deleon <lfdeleon98@gmail.com>
2021-10-27 15:51:35 -0700

    Implemented liking posts and changed the font on the message for the posts

--- src/components/Posts/Post/Post.js
+++ src/components/Posts/Post/Post.js
@@ -5,9 +5,12 @@ import ThumbUpAltIcon from '@material-ui/icons/ThumbUpAlt';
 import DeleteIcon from '@material-ui/icons/Delete';
 import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
 import moment from'moment';
+import { useDispatch } from 'react-redux';
+import { deletePost, likePost } from '../../../actions/posts';
 
 const Post = ({ post, setCurrentId }) => {
     const classes = useStyles();
+    const dispatch = useDispatch();
     return (
         <Card className={classes.card}>
             <CardMedia className={classes.media} image={post.selectedFile} title={post.title} />
@@ -25,18 +28,18 @@ const Post = ({ post, setCurrentId }) => {
             </div>
             <Typography className={classes.title} variant='h5' gutterBottom>{post.title}</Typography>
             <CardContent>
-                <Typography variant='h5' gutterBottom>{post.message}</Typography>
+                <Typography variant='body2' color='textSecondary' component='p'>{post.message}</Typography>
             </CardContent>
             <CardActions className={classes.cardActions} >
                 <Button 
                     size='small' 
                     color='primary' 
-                    onClick={() => {}}>
+                    onClick={() => dispatch(likePost(post._id))}>
                     <ThumbUpAltIcon fontSize='small'/>
                     Like
                     {post.likeCount}
                 </Button>
-                <DeleteIcon size='small' color='primary' onClick={() => {}} >
+                <DeleteIcon size='small' color='primary' onClick={() => dispatch(deletePost(post._id))} >
                     <ThumbUpAltIcon fontSize='small'/>
                     Delete
                 </DeleteIcon>
