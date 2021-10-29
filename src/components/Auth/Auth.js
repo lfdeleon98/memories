4b399ad37cf539b3799430aec270088f9c7f7cb8
lauren deleon <lfdeleon98@gmail.com>
2021-10-29 13:20:14 -0700

    initialized jwt login

--- src/components/Auth/Auth.js
+++ src/components/Auth/Auth.js
@@ -7,22 +7,31 @@ import { GoogleLogin } from 'react-google-login';
 import Icon from './icon'
 import { useDispatch } from 'react-redux';
 import { useHistory } from 'react-router-dom';
+import { signin, signup } from '../../actions/auth';
+
+const initialState = { firstName: '', lastName: '', email: '', password: '', confirmPassword: ''};
 
 const Auth = () => {
     const classes = useStyles();
     const [showPassword, setShowPassword] = useState(false);
-    const [isSignup, setIsSignup] = useState(false)
+    const [isSignup, setIsSignup] = useState(false);
+    const [formData, setFormData] = useState(initialState);
     const dispatch = useDispatch();
     const history = useHistory();
 
     const handleShowPassword = () => setShowPassword((prevShowPassword) => !prevShowPassword);
 
-    const handleSubmit = () => {
-
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (isSignup) {
+            dispatch(signup(formData, history));
+        } else {
+            dispatch(signin(formData, history))
+        }
     };
 
-    const handleChange = () => {
-
+    const handleChange = (e) => {
+        setFormData({ ... formData, [e.target.name]: e.target.value })
     };
 
     const switchMode = () => {
